be41817025b8939ccdab835521f604a85041fcce
yangjifei <1021967981@qq.com>
2024-01-18 09:54:17 +0800

    first check

--- liquidfun/Box2D/Box2D/Particle/b2ParticleAssemblyNeon.v8.s
+++ liquidfun/Box2D/Box2D/Particle/b2ParticleAssemblyNeon.v8.s
@@ -3,7 +3,7 @@
         .global   CalculateTags_Simd
 
 CalculateTags_Simd:
-        ld1r {v12.4s}, [x2] // q12 = inverseDiameter
+        ld1 {v12.4s}, [x2] // q12 = inverseDiameter
         mov w13, #0x100
         mov w14, #0x80000
         mov w15, #0x800
@@ -30,7 +30,7 @@ CalculateTags_Simd:
         fcvtzu v0.4s, v0.4s             // v0 = (uint32)(x * xScale + xOffset)
         sli v0.4s, v1.4s, #20
 
-        sub x1, x1, #4
+        sub w1, w1, #4
 
         // Write out, ignoring index.
         st1 {v0.4s}, [x3],#16   // write out tags// increment ptr //16*8=128
@@ -48,10 +48,6 @@ FindContacts_PostProcess:
         ldrh              w10, [x4, #2]
         ldrh              w11, [x4, #16]
         ldrh              w12, [x4, #18]
-        prfm              PLDL1KEEP, [x7, w9, SXTW #3]
-        prfm              PLDL1KEEP, [x7, w10, SXTW #3]
-        prfm              PLDL1KEEP, [x7, w11, SXTW #3]
-        prfm              PLDL1KEEP, [x7, w12, SXTW #3]
 
         ld4 {v0.4s, v1.4s, v2.4s, v3.4s},[x4],#64
 
@@ -67,10 +63,10 @@ FindContacts_PostProcess:
         ld1               {v9.4s, v10.4s, v11.4s}, [x8]
 
         // Load first four flags, 'or' them in pairs, then write to destination.
-        ldr               w9, [x7, x9, lsl #2]
-        ldr               w10, [x7, x10, lsl #2]
-        ldr               w11, [x7, x11, lsl #2]
-        ldr               w12, [x7, x12, lsl #2]
+        ldr               w9 , [x7, w9 , uxtw #2]
+        ldr               w10, [x7, w10, uxtw #2]
+        ldr               w11, [x7, w11, uxtw #2]
+        ldr               w12, [x7, w12, uxtw #2]
         orr               w9, w9, w10
         orr               w11, w11, w12
         str               w9, [x4, #16]
@@ -81,10 +77,6 @@ FindContacts_PostProcess:
         ldrh              w10, [x4, #34]
         ldrh              w11, [x4, #48]
         ldrh              w12, [x4, #50]
-        prfm              PLDL1KEEP, [x7, x9, lsl #3]
-        prfm              PLDL1KEEP, [x7, x10, lsl #3]
-        prfm              PLDL1KEEP, [x7, x11, lsl #3]
-        prfm              PLDL1KEEP, [x7, x12, lsl #3]
 
         // Calculate normal and weight.
         fmul v1.4s, v1.4s, v8.4s     // v1 = distSq / dist = dist
@@ -115,17 +107,17 @@ FindContacts_PostProcess:
         mov               x8, #12        // x8 = 12 = sizeof(FindContactInput)
 
         // Load next four flags, 'or' them in pairs, then write to destination.
-        ldr               w9, [x7, x9, lsl #2]
-        ldr               w10, [x7, x10, lsl #2]
-        ldr               w11, [x7, x11, lsl #2]
-        ldr               w12, [x7, x12, lsl #2]
+        ldr               w9 , [x7, w9 , uxtw #2]
+        ldr               w10, [x7, w10, uxtw #2]
+        ldr               w11, [x7, w11, uxtw #2]
+        ldr               w12, [x7, w12, uxtw #2]
         orr               w9, w9, w10
         orr               w11, w11, w12
         str               w9, [x4, #-24]
         str               w11, [x4, #-4]
 
         add               x3, x3, #16
-        add               x5, x5, #4          // numContacts += 4
+        add               w5, w5, #4          // numContacts += 4
 
         st1 {v9.4s, v10.4s, v11.4s}, [x4]
 
@@ -153,13 +145,13 @@ FindContactsFromChecks_Simd:
         stp x30, xzr, [sp, #-16]!
 
         // Load constants from registers and stack.
-        dup               v15.4s, w3  // v15 = particleDiameterSq
-        ldr               w12, [sp, #72]       // w12 = particleDiameterInv
-        dup               v14.4s, w12 // v14 = particleDiameterInv
-        ldr               x9, [sp, #88]    // x9 = contacts
-        ldr               x7, [sp, #80]    // x7 = flags
+        ld1r               {v15.4s}, [x3]  // v15 = particleDiameterSq
+        ldr               x12, [x4]       // r12 = particleDiameterInv
+        ld1r               {v14.4s}, [x12] // v14 = particleDiameterInv
+        ldr               x9, [x6]    // x9 = contacts//x6
+        ldr               x7, [x5]    // x7 = flags//x5
         ldr               x3, [x9, #0]     // x3 = out = contacts.data
-        ldr               w6, [x9, #8]     // w6 = contacts.capacity
+        ldr               w6, [x9, #12]     // w6 = contacts.capacity
         mov               x4, x3           // x4 = postProcess = outContacts
         mov               w5, #0           // w5 = numContacts
         sub               w6, w6, #8       // w6 = maxSafeContacts = capacity - 8
@@ -173,7 +165,7 @@ FindContactsFromChecks_Simd:
         // Load and calculate remaining constants.
         fmov              v12.4s, #1.0        // v12 = 1.0f splatted
         adr               x12, CONST_IS_CLOSE_TABLE_INDICES
-        ld1               {v13.4s}, [x12]     // v13 = *CONST_IS_CLOSE_TABLE_INDICES
+        ld1               {v13.8b}, [x12]     // v13 = *CONST_IS_CLOSE_TABLE_INDICES
 
 
 
@@ -223,13 +215,13 @@ FindContactsFromChecks_Simd:
         //   isClose = distBtParticlesSq < particleDiameterSq
         fcmlt v8.4s, v1.4s, v15.4s    // v8 = isClose
         tbl v8.8b, {v8.16b}, v13.8b  // v16 = isClose(packed)
-        mov w12, v16.s[0]            // v16[0] ==> w12
+        mov w12, v8.s[0]            // v16[0] ==> w12
 
         // If not enough space in output array, grow it.
         // This is a heavy operation, but should happen rarely.
         b.le              .L_FindContacts_Output
-        ldr               x9, [sp, #44]        // x9 = contacts
-        str               w5, [x9, #4]         // contacts.count = numContacts
+        ldr               x9, [x6]        // x9 = contacts
+        str               w5, [x9, #8]         // contacts.count = numContacts
         ldr               x10, [x9, #0]        // x10 = contacts.data
         stp               x0, x1, [sp, #-16]!
         stp               x2, x3, [sp, #-16]!
@@ -250,7 +242,7 @@ FindContactsFromChecks_Simd:
 
         // The output array was reallocated, so update 'out', 'postProcess' and
         // 'maxSafeContacts' pointers.
-        ldr               w6, [x9, #8]         // w6 = contacts.capacity
+        ldr               w6, [x9, #12]         // w6 = contacts.capacity
         ldr               x9, [x9, #0]         // x9 = contacts.data
         sub               x9, x9, x10          // x9 = data buffer offset
         sub               w6, w6, #8           // w6 = maxSafeContacts
@@ -273,37 +265,35 @@ FindContactsFromChecks_Simd:
         trn1 v1.2d, v5.2d, v7.2d  // v1: a1, b1, c1, d1
         trn2 v3.2d, v5.2d, v7.2d  // v3: a3, b3, c3, d3
 
-    // Store results to memory, but only results that are close
-    tst   w12, #0xFF
-    b.ne  .L_Skip_1st_Contact
-    st1 {v0.4s}, [x3],#16
+        // Store results to memory, but only results that are close
+        tst   w12, #0xFF
+        b.ne  .L_Skip_1st_Contact
+        st1 {v0.4s}, [x3],#16
 
 .L_Skip_1st_Contact:
-    tst   w12, #0xFF00
-    b.ne  .L_Skip_2nd_Contact
-    st1 {v1.4s}, [x3],#16
+        tst   w12, #0xFF00
+        b.ne  .L_Skip_2nd_Contact
+        st1 {v1.4s}, [x3],#16
 
 .L_Skip_2nd_Contact:
-    tst   w12, #0xFF0000
-    b.ne  .L_Skip_3rd_Contact
-    st1 {v2.4s}, [x3],#16
+        tst   w12, #0xFF0000
+        b.ne  .L_Skip_3rd_Contact
+        st1 {v2.4s}, [x3],#16
 
 .L_Skip_3rd_Contact:
-    tst   w12, #0xFF000000
-    b.ne  .L_Skip_4th_Contact
-    st1 {v3.4s}, [x3],#16
+        tst   w12, #0xFF000000
+        b.ne  .L_Skip_4th_Contact
+        st1 {v3.4s}, [x3],#16
 
 .L_Skip_4th_Contact:
-    // post-process the last four elements that have been output
-    // w12 = 5th element to not be post-processed yet
-    add   w12, w4, #64         // w12 = nextPostProcess
-    cmp   w3, w12
-    b.ge  FindContacts_PostProcess
-
-    // decrement loop counter // sets the 'gt' flag used in 'bgt' below
-    subs  w2, w2, #1
-    bgt   .L_FindContacts_MainLoop
-
+        // post-process the last four elements that have been output
+        // w12 = 5th element to not be post-processed yet
+        add   x12, x4, #64         // w12 = nextPostProcess
+        cmp   x3, x12
+        b.ge  FindContacts_PostProcess  
+        // decrement loop counter // sets the 'gt' flag used in 'bgt' below
+        subs  w2, w2, #1
+        bgt   .L_FindContacts_MainLoop
 
 .L_FindContacts_PostProcessRemainingItems:
         // If at least one output item needs post-processing, do it.
@@ -312,7 +302,7 @@ FindContactsFromChecks_Simd:
 
         // w12/16 = num extra contacts to process
         add               w5, w5, w12, lsr #4  // numContacts += num extra
-        stp               w5, w30, [sp, #-16]! // Save numContacts and lr
+        stp               x5, x30, [sp, #-16]! // Save numContacts and lr
 
         // Ensure indices past end of array are zeroed out.
         // We process 4 contacts in FindContacts_PostProcess, even if we only
@@ -323,12 +313,12 @@ FindContactsFromChecks_Simd:
         str               w12, [x3, #32]
 
         bl                FindContacts_PostProcess
-        ldp               w5, w30, [sp], #16   // Restore numContacts and lr
+        ldp               x5, x30, [sp], #16   // Restore numContacts and lr
 
 .L_FindContacts_Return:
         // Set the final number of contacts in the output buffer.
-        ldr               x9, [sp, #88]        // x9 = contacts
-        str               w5, [x9, #4]         // contacts.count = numContacts
+        ldr               x9, [x6]        // x9 = contacts
+        str               w5, [x9, #8]         // contacts.count = numContacts
 
         // Return by popping the original lr into pc.
         ldp x30, xzr, [sp], #16
